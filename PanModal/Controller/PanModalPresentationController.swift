ac7a458b063dcbd155cbe6710b8b7e6b99570580
Michał Osadnik <micosa97@gmail.com>
2020-06-10 02:27:35 +0200

    Update PanModalPresentationController.swift

--- PanModal/Controller/PanModalPresentationController.swift
+++ PanModal/Controller/PanModalPresentationController.swift
@@ -468,7 +468,8 @@ private extension PanModalPresentationController {
          Set the appropriate contentInset as the configuration within this class
          offsets it
          */
-        scrollView.contentInset.bottom = presentingViewController.bottomLayoutGuide.length
+      
+      scrollView.contentInset.bottom = (presentable?.ignoreBottomOffset ?? false) ? 0 : presentingViewController.bottomLayoutGuide.length
 
         /**
          As we adjust the bounds during `handleScrollViewTopBounce`
